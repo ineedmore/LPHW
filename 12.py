e0c8d4b39d4f14d789b9865b6932620de11d5259
Anvar <anvar.big@gmail.com>
2019-03-25 18:09:42 +0300

    Initial commit

---
+++ 12.py
@@ -0,0 +1,4 @@
+age = input("What is your age?")
+height = input("How tall are you?")
+weight = input("How heavy are you?")
+print(f"So, you are {age} years old, {height} cm tall and {weight} kg heavy")
