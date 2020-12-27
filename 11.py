9b36a370fde8b28cc20920d9a1a3b93699bf31d4
ineedmore <>
2020-12-27 22:10:04 +0300

    alles gut

--- 11.py
+++ 11.py
@@ -1,7 +1,1 @@
-print("How old are you?", end=' ')
-age = input()
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weight?", end=' ')
-weight = input()
-print(f"So, you're {age} years old, {height} cm tall and {weight} kg heavy.")
+you are awesome temp/something/awesome.txt
